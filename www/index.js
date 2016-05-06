3b99f2cbfb1ffba2293204d8f70cbd9b96482f90
Sralladah <Mimo10@onet.eu>
2016-05-06 09:03:45 +0100

    HANGMAN BETA v2.4
    The game actually works now!
    the difficulty settings do not work, always on easy.

--- www/index.js
+++ www/index.js
@@ -1,51 +1,43 @@
 // JavaScript Document
 
-var usedLetters = [];
-var index;
-var queuedLetter;
 var image;
-var lives = 6; //amount of lives
+var lives = 10; //amount of lives
 var word; //will be used to keep the word in play
+var currentGuess = "";
+var letterFound = false; //used to determinate if a letter was found after a guess, so no lives are taken away
+var usedGuesses = [];
+var used = false;
+var hiddenWord = [];
+
+//The words list is declared full only because i can't get difficulty selection working.
+var wordsList = ["LAMP", "ROAD", "SOFA", "LADY", "SNOW", "SPACE", "NOISE", "DOGGY", "HOUSE", "STEEL", "STEREO", "PILLOW", "CEREAL", "BUTTON", "CIRCLE", "BALLOON", "STATION", "DRAWING", "BICYCLE", "FLOWERS"];
+
+var endGame;
+var wordToGuess = [];
 
 
-function easy() {
-    var wordsList = [lamp, road, sofa, lady, snow, space, noise, doggy, house, steel, stereo, pillow, cereal, button, circle, balloon, station, drawing, bicycle, flowers]
+
+//TODO difficulty selection not working
+//set the words list according to the difficulty selected
+/*function easy() {
+    wordsList = ["LAMP", "ROAD", "SOFA", "LADY", "SNOW", "SPACE", "NOISE", "DOGGY", "HOUSE", "STEEL", "STEREO", "PILLOW", "CEREAL", "BUTTON", "CIRCLE", "BALLOON", "STATION", "DRAWING", "BICYCLE", "FLOWERS"];
 }
 
 function medium() {
-    var wordsList = [painting, moneybag, sapphire, aromatic, captions, telephone, godfather, offensive, efficient, telescope, bottomless, whirlwinds, speciality, programing, television, unbreakable, application, bodybuilder, electricity, masterpiece]
-}
+    wordsList = ["PAINTING", "MONEYBAG", "SAPPHIRE", "AROMATIC", "CAPTIONS", "TELEPHONE", "GODFATHER", "OFFENSIVE", "EFFICIENT", "TELESCOPE", "BOTTOMLESS", "WHIRLWINDS", "SPECIALITY", "PROGRAMING", "TELEVISION", "UNBREAKABLE", "APPLICATION", "BODYBUILDER", "ELECTRICITY", "MASTERPIECE"]
+ }
 
 function hard() {
-    var wordsList = [astronomical, vocabularies, thanksgiving, stockholders, rustproofing, extendability, incorporation, nonfunctional, perpendicular, recommendable, miscalculating, ambassadorship, deconstructive, impracticality, illegitimately, bioluminescence, wrongheadedness, troubleshooting, survivabilities, procrastination/'']
-}
+    wordsList = ["ASTRONOMICAL", "VOCABULARIES", "THANKSGIVING", "STOCKHOLDERS", "RUSTPROOFING", "EXTENDABILITY", "INCORPORATION2", "NONFUNCTIONAL", "PERPENDICULAR", "RECOMMENDABLE", "MISCALCULATING", "AMBASSADORSHIP", "DECONSTRUCTIVE", "IMPRACTICALITY", "ILLEGITIMATELY", "BIOLUMINESCENCE", "WRONGHEADEDNESS", "TROUBLESHOOTING", "SURVIVABILITIES", "PROCRASTINATION"]
+ }*/
 
-function hideLetter() {
-    for (index = 0; index < usedLetters.length; index++) {
-        queuedLetter = usedLetters[index];
-        image = document.getElementById(queuedLetter);
-        image.src = "Images/btnUsed.jpg";
-    }
-    document.getElementById("display").innerHTML = usedLetters.join("");
-}
-
-function resetLetters() {
-    for (index = 0; index < usedLetters.length; index++) {
-        queuedLetter = usedLetters[index];
-        image = document.getElementById(queuedLetter);
-        image.src = "Images/btn" + queuedLetter + ".jpg";
-    }
-    usedLetters = [];
-    document.getElementById("display").innerHTML = usedLetters;
-}
-
+//First, pick a word from the array
 function newWord()
 {
     var ranNum = 100;
 
-//Math.random * 100 gives numbers between 0 and 100
-//So i made the loop until i got a number that fits the array
-    while(ranNum > wordsList.length && ranNum > 1)
+//Loop until number that fits the array
+    while(ranNum > wordsList.length && ranNum > 0)
     {
         ranNum = Math.random()*100;
     }
@@ -54,254 +46,190 @@ function newWord()
 
 }
 
-var wordToGuess = [];
-wordToGuess = newWord();
+//This function allows to reset the game after finishing
+function newGame() {
+    lives = 10;
+    usedGuesses = [];
+    hiddenWord = [];
+    wordToGuess = newWord();
 
-
-
-function aClick() {
-    usedLetters.push("A");
-    hideLetter();
-}
-function bClick() {
-    usedLetters.push("B");
-    hideLetter();
-}
-function cClick() {
-    usedLetters.push("C");
-    hideLetter();
-}
-function dClick() {
-    usedLetters.push("D");
-    hideLetter();
-}
-function eClick() {
-    usedLetters.push("E");
-    hideLetter();
-}
-function fClick() {
-    usedLetters.push("F");
-    hideLetter();
-}
-function gClick() {
-    usedLetters.push("G");
-    hideLetter();
-}
-function hClick() {
-    usedLetters.push("H");
-    hideLetter();
-}
-function iClick() {
-    usedLetters.push("I");
-    hideLetter();
-}
-function jClick() {
-    usedLetters.push("J");
-    hideLetter();
-}
-function kClick() {
-    usedLetters.push("K");
-    hideLetter();
-}
-function lClick() {
-    usedLetters.push("L");
-    hideLetter();
-}
-function mClick() {
-    usedLetters.push("M");
-    hideLetter();
-}
-function nClick() {
-    usedLetters.push("N");
-    hideLetter();
-}
-function oClick() {
-    usedLetters.push("O");
-    hideLetter();
-}
-function pClick() {
-    usedLetters.push("P");
-    hideLetter();
-}
-function qClick() {
-    usedLetters.push("Q");
-    hideLetter();
-}
-function rClick() {
-    usedLetters.push("R");
-    hideLetter();
-}
-function sClick() {
-    usedLetters.push("S");
-    hideLetter();
-}
-function tClick() {
-    usedLetters.push("T");
-    hideLetter();
-}
-function uClick() {
-    usedLetters.push("U");
-    hideLetter();
-}
-function vClick() {
-    usedLetters.push("V");
-    hideLetter();
-}
-function wClick() {
-    usedLetters.push("W");
-    hideLetter();
-}
-function xClick() {
-    usedLetters.push("X");
-    hideLetter();
-}
-function yClick() {
-    usedLetters.push("Y");
-    hideLetter();
-}
-function zClick() {
-    usedLetters.push("Z");
-    hideLetter();
+    while (hiddenWord.length < wordToGuess.length) {
+        hiddenWord.push("-");
+    }
+   return hiddenWord;
 }
 
-/*
-var gameOn = true;
-var hiddenWord = [];
-var playAgain;
-
-//We don't want the player to see the word, so i created an array what will show dashes instead of the word
-//As the player guesses the word, the dashes will become letters from wordToGuess array
-while (hiddenWord.length < wordToGuess.length)
-{
-    hiddenWord.push("-");
-}
-
-//Introduction and game start
-var currentGuess;
-var letterFound = false; //used to determinate if a letter was found after a guess, so no lives are taken away
-var usedGuesses = [];
-var used = false;
-
-while (gameOn) //Keep the game going while the player haven't guessed the word and still has lives
-{
-    //Notify the player of how many lives he has left, and let the player guess again
-    io.output("\nYou have " + lives + " lives\n" + hiddenWord);
-    currentGuess = io.input("\n>");
-
-
-    //Check if the player already used this letter.
-    for (var i = 0; i < usedGuesses.length + 1; i++)
-    {
-        if (usedGuesses[i] == currentGuess)
-        {
-            io.output("You have already used that");
+function guessCheck() {
+    //1. Check if the player already used this letter, if so skip guessCheck.
+    for (var i = 0; i < usedGuesses.length + 1; i++) {
+        if (usedGuesses[i] == currentGuess) {
             used = true;
             break;
         }
     }
-
-
-
-    //If the letter was NOT used, search for a match with the word in play
-    if (used == false)
-    {
-        //Check if the player guesses the entire word, if yes ask if the player wants to play again, else end the game
-        if (wordToGuess.join("") == currentGuess)
-        {
-            io.output("Congratulations, you guessed the word!");
-            playAgain = io.input("\nWould you like to play again?\n\n> "); playAgain = playAgain.toLowerCase(); //just in case the player used capitals
-            if (playAgain == "yes")
-            {
-                restartGame();
+    //if letter not used before: 
+    // 2. check if guess is correct, if so display it in the correct position, if not deduct a life
+    // 3. check if the player guessed all the letters, if so end game, if not continue
+    // 4. check if the player still has lives, if not end game.
+    // 5. mark the letter as used
+    // 6. hide the button to indicate it's been used
+    // 7. prepare game for next guess
+    if (used == false) {
+        for (i = 0; i < wordToGuess.length + 1; i++) { //A loop to check each letter one at a time
+            if (wordToGuess[i] == currentGuess){
+                hiddenWord.splice(i, 1, currentGuess);//if guess was right reveal the letter in the hidden word
+                letterFound = true;
+                document.getElementById("display").innerHTML = hiddenWord.join("");
             }
-            else
-            {
-                gameOn = false;
-            }
-        }
 
-        //Check if the guessed letter is anywhere in the word
-        else
-        {
-            for (i = 0; i < wordToGuess.length + 1; i++) //A loop to check each letter one at a time
-            {
-                if (wordToGuess[i] == currentGuess)
-                {
-                    hiddenWord.splice(i, 1, currentGuess);
-                    letterFound = true
-                }
-
-                //If the search for the letter is complete, and none matched, take a life away
-                else if (i == wordToGuess.length && letterFound == false)
-                {
-                    lives--;
-                }
-
-            }
-        }
-
-
-        //After a match was made or not, add the guessed letter or word to an array
-        //then reset letterFound and check if the player got all the letters and still has lives to continue the game
-        usedGuesses.push(currentGuess); //Stores the used letter or word so the player doesn't loose a life when using it again
-        letterFound = false; //Setting letterFound back to false to "restart" the round
-
-        //Checking if the player guessed the word and won the game, if yes ask if the player wants to play again
-        if (hiddenWord.join("") == wordToGuess.join(""))
-        {
-            io.output("\nYou have " + lives + " lives\n" + wordToGuess + "\n\nCongratulations, you guessed the word!");
-            playAgain = io.input("\nWould you like to play again?\n\n> "); playAgain = playAgain.toLowerCase(); //just in case the player used capitals
-            if (playAgain == "yes")
-            {
-                restartGame();
-            }
-            else
-            {
-                gameOn = false;
-            }
-        }
-
-        //Checking if the player has run out of lives, if yes ask if the player wants to play again
-        if (lives == 0)
-        {
-            io.output("You're out of lives\nThe word was '" + wordToGuess.join("") + "'\n\nGAME OVER");
-            playAgain = io.input("\nWould you like to play again?\n\n> "); playAgain = playAgain.toLowerCase(); //just in case the player used capitals
-            if (playAgain == "yes")
-            {
-                restartGame();
-            }
-            else
-            {
-                gameOn = false;
+            //if guess was wrong, deduct a life
+            else if (i == wordToGuess.length && letterFound == false) {
+                lives--;
             }
 
         }
+
+        //if player guessed all the letters, end game
+        if (hiddenWord.join("") == word) {
+            endGame = confirm("You Won! Go back to menu?");
+            if (endGame == true) {
+                window.location.href = "index.html";
+            }
+        }
+
+        //if player lost all their lives, end game
+        else if(lives == 0){
+            if (endGame == true) {
+                window.location.href = "index.html";
+            }
+        }
+
+        //mark the letter as used, hide the button and reset letterFound status to prepare for next guess
+        usedGuesses.push(currentGuess);
+        image = document.getElementById(currentGuess);
+        image.src = "Images/btnUsed.jpg";
+        letterFound = false
     }
-    //If the player used a letter that he used before, it skips the entire block of code and then
-    //gets set back to false in order to "reset" the round
+    //if letter was used and guessCheck was skipped, reset used status for next guess
     used = false;
 }
 
-//////////////////////////////////
- Restarting the game if the player wants to play again
- //////////////////////////////////
-
- This function is used to:
- * Restart lives
- * Clear usedGuesses
- * Clear wordToGuess
- * Clear hiddenWord
- * Pick a new word
-function restartGame()
-{
-    lives = 6;
-    usedGuesses = [];
-    wordToGuess = [];
-    hiddenWord = [];
-    wordToGuess = newWord();
-
-    while (hiddenWord.length < wordToGuess.length)
-    {
-        hiddenWord.push("-");
+/*
+ //this is only a debugging feature, allows to add a special button that resets used letters array
+ //to check guessCheck works properly
+function resetLetters() {
+    for (index = 0; index < usedGuesses.length; index++) {
+        queuedLetter = usedGuesses[index];
+        image = document.getElementById(queuedLetter);
+        image.src = "Images/btn" + queuedLetter + ".jpg";
     }
-} */
+    usedGuesses = [];
+    document.getElementById("display").innerHTML = usedGuesses;
+}
+*/
+
+//These are the functions for each button, i tried to find a more efficient way to do this but couldn't because of HTML
+function aClick() {
+    currentGuess = "A";
+    guessCheck();
+}
+function bClick() {
+    currentGuess = "B";
+    guessCheck();
+}
+function cClick() {
+    currentGuess = "C";
+    guessCheck();
+}
+function dClick() {
+    currentGuess = "D";
+    guessCheck();
+}
+function eClick() {
+    currentGuess = "E";
+    guessCheck();
+}
+function fClick() {
+    currentGuess = "F";
+    guessCheck();
+}
+function gClick() {
+    currentGuess = "G";
+    guessCheck();
+}
+function hClick() {
+    currentGuess = "H";
+    guessCheck();
+}
+function iClick() {
+    currentGuess = "I";
+    guessCheck();
+}
+function jClick() {
+    currentGuess = "J";
+    guessCheck();
+}
+function kClick() {
+    currentGuess = "K";
+    guessCheck();
+}
+function lClick() {
+    currentGuess = "L";
+    guessCheck();
+}
+function mClick() {
+    currentGuess = "M";
+    guessCheck();
+}
+function nClick() {
+    currentGuess = "N";
+    guessCheck();
+}
+function oClick() {
+    currentGuess = "O";
+    guessCheck();
+}
+function pClick() {
+    currentGuess = "P";
+    guessCheck();
+}
+function qClick() {
+    currentGuess = "Q";
+    guessCheck();
+}
+function rClick() {
+    currentGuess = "R";
+    guessCheck();
+}
+function sClick() {
+    currentGuess = "S";
+    guessCheck();
+}
+function tClick() {
+    currentGuess = "T";
+    guessCheck();
+}
+function uClick() {
+    currentGuess = "U";
+    guessCheck();
+}
+function vClick() {
+    currentGuess = "V";
+    guessCheck();
+}
+function wClick() {
+    currentGuess = "W";
+    guessCheck();
+}
+function xClick() {
+    currentGuess = "X";
+    guessCheck();
+}
+function yClick() {
+    currentGuess = "Y";
+    guessCheck();
+}
+function zClick() {
+    currentGuess = "Z";
+    guessCheck();
+}
